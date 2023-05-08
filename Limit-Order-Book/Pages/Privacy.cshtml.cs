0262854568364f22b9e02878b2ceed0a8f78c701
Tebogo Goqo <tebogoerickgoqo@gmail.com>
2023-05-08 12:31:37 +0200

    Add project files.

---
+++ Limit-Order-Book/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Limit_Order_Book.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
