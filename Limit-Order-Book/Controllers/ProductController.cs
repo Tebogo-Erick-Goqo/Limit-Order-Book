b5f6ed4393bbb73f1bdacf6d40f9bbbf9ab483bb
Tebogo Goqo <tebogoerickgoqo@gmail.com>
2023-05-12 15:28:43 +0200

    commit

--- Limit-Order-Book/Controllers/ProductController.cs
+++ Limit-Order-Book/Controllers/ProductController.cs
@@ -21,5 +21,30 @@ namespace Limit_Order_Book.Controllers
             ViewBag.orders = db.Orders.ToList();
             return View();
         }
+
+        [HttpGet]
+        [Route("add")]
+        public IActionResult Add()
+        {
+            return View("Add", new Orders());
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add(Orders orders)
+        {
+            db.Orders.Add(orders);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            db.Orders.Remove(db.Orders.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
